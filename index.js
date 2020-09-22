37cfdf63e89bad2575b5c762d82202a9c1d13d1a
Reynald Liandra <reynald.liandra@kreatifcity.com>
2020-09-22 11:40:57 +0700

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+const docx = require("docx-templates");
+const libre = require("libreoffice-convert");
+const fs = require("fs");
+const path = require("path")
+
+const template = fs.readFileSync('template.docx');
+
+docx.createReport({
+    template,
+    data: {
+        name: 'John',
+    },
+    cmdDelimiter: ['${', '}']
+}).then(buffer => {
+
+fs.writeFileSync(path.resolve(__dirname,"report.docx"), buffer)
+
+    libre.convert(buffer, ".pdf", undefined, (err, done) => {
+        if (err) {
+            console.log(`Error converting file: ${err.stack}`);
+            return;
+        }
+    
+        // Here in done you have pdf file which you can save or transfer in another stream
+        fs.writeFileSync(path.resolve(__dirname,"report.pdf"), done);
+    });
+})
+.catch(error => {
+    console.log(error.stack)
+});
+
